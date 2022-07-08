713933ca48bb9d2e269917ce7d749cd6d6dec97a
awesomejang <uljang1995@gmail.com>
2022-07-08 23:57:35 +0900

    user 게정 조회 로직 수정
    HttpStatus 201 로 리턴코드변경

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,15 +1,18 @@
 package com.example.restfulwebservice.user;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserController {
@@ -30,7 +33,14 @@ public class UserController {
 	}
 	
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
-		User save = service.save(user);
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				                                  .path("/{id}")
+				                                  .buildAndExpand(savedUser.getId())
+				                                  .toUri();
+		return ResponseEntity.created(location).build(); //created = 201 코드 Header에 Location속성의 value
+		//ResponseEntity.notFound().build();
 	}
 }
