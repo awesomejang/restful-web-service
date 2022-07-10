cecb045488b964a4ef917532f2fb60760364cc62
awesomejang <uljang1995@gmail.com>
2022-07-10 23:58:23 +0900

    범용 @ControllerAdvice 추가

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -29,7 +29,12 @@ public class UserController {
 	
 	@GetMapping("users/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+		
+		if(user == null) {
+			throw new UserNotFoundException(String.format("ID[%s] not fount", id));
+		}
+		return user;
 	}
 	
 	@PostMapping("/users")
